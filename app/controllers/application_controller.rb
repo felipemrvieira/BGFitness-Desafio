8990e3ccb1dc1fbc0fae4fb3eebf7465f9fed2cf
felipemaciel <felipemrvieira@gmail.com>
2018-07-31 11:36:20 -0300

    ajustes nas views de login(devise) e links da nav

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,16 +2,16 @@ class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
   before_action :authenticate_admin!
 
-  #layout :layout_by_resource
+  layout :layout_by_resource
 
   # Layout per resource_name
-  # def layout_by_resource
-  #   if devise_controller?
-  #     "devise-layout"
-  #   else
-  #     "application"
-  #   end
-  # end
+  def layout_by_resource
+    if devise_controller?
+      "devise-layout"
+    else
+      "application"
+    end
+  end
 
  
 
